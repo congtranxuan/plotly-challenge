15f057735b024e60da6166ffc003a8a07bd879cf
congtranxuan <congtranxuan@gmail.com>
2019-09-27 14:03:03 -0500

    update

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -94,11 +94,8 @@ function buildMetadata(sample) {
           // ];
 
           console.log(data);
-=]
 
           var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-
-         
 
           Plotly.newPlot("gauge", data, layout);  
 
